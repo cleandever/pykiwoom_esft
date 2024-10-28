9a3ccfc86bf8546e5686765938cb796b8443e644
cleandever <cleandever@gmail.com>
2024-10-29 05:21:26 +0900

    modified exiting process

--- src/util/helper.py
+++ src/util/helper.py
@@ -1,7 +1,10 @@
 import datetime
 import os
+import sys
 
 import psutil as psutil
+
+from src.util.logger import Logger
 
 
 class Helper:
@@ -40,6 +43,41 @@ class Helper:
 
     @classmethod
     def terminate_current_process(cls):
-        current_system_pid = os.getpid()
-        ThisSystem = psutil.Process(current_system_pid)
-        ThisSystem.terminate()
+        """현재 프로세스와 모든 자식 프로세스를 안전하게 종료"""
+        try:
+            current_process = psutil.Process(os.getpid())
+
+            # 1. 자식 프로세스 종료
+            children = current_process.children(recursive=True)
+            for child in children:
+                Helper.terminate_process_safely(child, is_child=True)
+
+            # 2. 현재 프로세스 종료
+            Helper.terminate_process_safely(current_process, is_child=False)
+
+        except Exception as e:
+            Logger.write(f'프로세스 종료 작업 실패: {str(e)}')
+            sys.exit(1)
+
+    @classmethod
+    def terminate_process_safely(cls, process, is_child=True):
+        """단일 프로세스를 안전하게 종료하는 함수
+
+        Args:
+            process (psutil.Process): 종료할 프로세스 객체
+            is_child (bool): 자식 프로세스 여부
+        """
+        process_type = "child" if is_child else "parent"
+        Logger.write(f'{process_type} 프로세스 강제종료 - {process.name()} (PID: {process.pid})')
+        try:
+            process.terminate()
+            process.wait(timeout=3)
+
+        except psutil.TimeoutExpired:
+            Logger.write(f'{process_type} 프로세스 응답 없음 - 강제 종료 시도')
+            process.kill()
+        except psutil.NoSuchProcess:
+            Logger.write(f'{process_type} 프로세스가 이미 종료됨')
+        except Exception as e:
+            Logger.write(f'{process_type} 프로세스 종료 중 예외 발생: {str(e)}')
+            raise
